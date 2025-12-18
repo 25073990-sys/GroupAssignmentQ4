e3d8894873e723b358796f98a9814269b650ed3c
Kan Hoi Yeng <237968832+hyjanesa88@users.noreply.github.com>
2025-12-18 14:31:51 +0800

    Read employees.csv in FileService

--- src/services/FileService.java
+++ src/services/FileService.java
@@ -1,4 +1,41 @@
 package services;
 
+import models.Employee;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
 public class FileService {
+    public static List<Employee> loadEmployees(){
+        List<Employee> employees = new ArrayList<>();
+
+        try(BufferedReader br = new BufferedReader(new FileReader("data/employees.csv"))){
+            String line;
+            boolean firstLine = true;
+
+            while((line = br.readLine()) != null){
+                if(firstLine){
+                    firstLine = false;
+                    continue;
+                }
+                String []data = line.split(",");
+                if(data.length >= 4){
+                    Employee emp = new Employee(
+                            data[0].trim(), // Employee ID
+                            data[1].trim(), // EmployeeName
+                            data[2].trim(), // Role
+                            data[3].trim() // Password
+                    );
+                    employees.add(emp);
+                }
+            }
+        System.out.println("Loaded" + employees.size() + " employees with roles");
+
+    }catch (Exception e){
+        System.out.println("Error loading CSV: " + e.getMessage());
+        }
+        return employees;
+    }
 }
